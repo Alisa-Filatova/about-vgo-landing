7c62ca9439681730d6d80d24f59fdd12cb0cae4c
Alisa Filatova <alicefill@bk.ru>
2019-08-31 16:41:22 +0300

    updated

--- index.js
+++ index.js
@@ -1,120 +1,105 @@
-(function() {
+const IS_SAFARI = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const BODY = IS_SAFARI ? document.body : document.documentElement;
 
-  var IS_SAFARI = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-  var BODY = IS_SAFARI ? document.body : document.documentElement;
+const INCREMENT = 16;
+let BORDER = 0;
+let IS_WIDE_SCREEN = false;
+let resizeTimeout;
 
-  var STOP = 0;
-  var INCREMENT = 16;
-  var SHIFT_CORRECTION = 50;
-  var BORDER = 0;
-  var IS_WIDE_SCREEN = false;
+const bg = document.getElementById('header-background');
+const droid = document.getElementById('droid');
+const birds = document.getElementById('birds');
+const monster = document.getElementById('monster');
+const robot = document.getElementById('robot');
+const advantages = document.querySelectorAll('.advantage');
 
-  var resizeTimeout;
+const easeInOutQuad = (t, b, c, d) => {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
 
-  var bg = document.getElementById('header-background');
-  var droid = document.getElementById('droid');
-  var birds = document.getElementById('birds');
-  var monster = document.getElementById('monster');
-  var robot = document.getElementById('robot');
+const scrollTo = (to, duration) => {
+  const start = BODY.scrollTop;
+  let currentTime = 0;
 
-  var heroes = document.getElementById('heroes');
-  var hero = document.getElementById('hero');
-  var stop = document.getElementById('stop');
+  const animateScroll = () => {
+    currentTime += INCREMENT;
+    BODY.scrollTop = easeInOutQuad(currentTime, start, to - start, duration);
+    if (currentTime < duration) setTimeout(animateScroll, INCREMENT);
+  };
 
-  var advantages = document.querySelectorAll('.advantage');
+  animateScroll();
+};
 
-  function getInitPositions() {
-    STOP = stop.getBoundingClientRect().bottom - (hero.getBoundingClientRect().bottom - SHIFT_CORRECTION);
+const onScroll = () => {
+  advantages.forEach(function (advantage) {
+    const top = advantage.getBoundingClientRect().top;
+    if (top < BORDER) {
+      advantage.classList.add('show');
+    }
+  })
+};
+
+const transform = (target, x, y, transform = '') => {
+  if (!target) return;
+  target.style.transform = `translate3d(${x}px, ${y}px, 0) ${transform}`;
+};
+
+const animateHeader = (x, y) => {
+  transform(bg, x / 4, y / 4, 'scale(1.1)');
+  transform(birds, x / 2, y / 2);
+  transform(droid, x / 2, y / 2);
+  transform(monster, x / 1.2, y / 1.2);
+  transform(robot, x, y);
+};
+
+const onDeviceOrientation = (e) => {
+  if (IS_WIDE_SCREEN) return;
+
+  const landscape = window.orientation === 90 || window.orientation === -90;
+  const x = Math.floor(40 * (landscape ? e.beta : e.gamma) / 100);
+  const y = Math.floor(40 * (landscape ? e.gamma : e.beta) / 100);
+
+  animateHeader(Math.floor(40 * x / 100), Math.floor(40 * y / 100));
+};
+
+const calcMouseShift = (pos, size, limit) => {
+  const half = size / 2;
+  return (pos - half) * (limit * 100 / half) / 100;
+};
+
+const onMouseMove = (event) => {
+  const x = calcMouseShift(event.pageX, window.innerWidth, 40);
+  const y = calcMouseShift(event.pageY, window.innerHeight, 40);
+  animateHeader(x, y);
+};
+
+const onDocumentClick = (event) => {
+  if (event.isTrusted !== true) return;
+  if (event.target.classList.contains('arrow-down-btn')) {
+    scrollTo(window.innerHeight, 1600);
   }
+};
 
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
+const init = () => {
+  IS_WIDE_SCREEN = window.innerWidth >= 1024;
+  BORDER = window.innerHeight - (window.innerHeight / 3);
+  if (BODY.scrollTop > 0) onScroll();
+};
 
-  function scrollTo(to, duration) {
-    var start = BODY.scrollTop;
-    var currentTime = 0;
+const onResize = () => {
+  window.clearTimeout(resizeTimeout);
+  resizeTimeout = window.setTimeout(function() {
+    init();
+  }, 300);
+};
 
-    var animateScroll = function () {
-      currentTime += INCREMENT;
-      BODY.scrollTop = easeInOutQuad(currentTime, start, to - start, duration);
-      if (currentTime < duration) setTimeout(animateScroll, INCREMENT);
-    };
+setTimeout(function() { init() }, 16);
 
-    animateScroll();
-  }
-
-  function onScroll() {
-    advantages.forEach(function (advantage) {
-      var top = advantage.getBoundingClientRect().top;
-      if (top < BORDER) {
-        advantage.classList.add('show');
-      }
-    })
-  }
-
-  function transform (target, x, y, transform = '')  {
-    if (!target) return;
-    target.style.transform = `translate3d(${x}px, ${y}px, 0) ${transform}`;
-  }
-
-  function animateHeader (x, y) {
-    transform(bg, x / 4, y / 4, 'scale(1.1)');
-    transform(birds, x / 2, y / 2);
-    transform(droid, x / 2, y / 2);
-    transform(monster, x / 1.2, y / 1.2);
-    transform(robot, x, y);
-  }
-
-  function onDeviceOrientation(e) {
-    if (IS_WIDE_SCREEN) return;
-
-    var landscape = window.orientation === 90 || window.orientation === -90;
-    var x = Math.floor(40 * (landscape ? e.beta : e.gamma) / 100);
-    var y = Math.floor(40 * (landscape ? e.gamma : e.beta) / 100);
-
-    animateHeader(Math.floor(40 * x / 100), Math.floor(40 * y / 100));
-  }
-
-  function calcMouseShift(pos, size, limit) {
-    const half = size / 2;
-    return (pos - half) * (limit * 100 / half) / 100;
-  }
-
-  function onMouseMove(event) {
-    const x = calcMouseShift(event.pageX, window.innerWidth, 40);
-    const y = calcMouseShift(event.pageY, window.innerHeight, 40);
-    animateHeader(x, y);
-  }
-
-  function onDocumentClick(e) {
-    if (e.isTrusted !== true) return;
-    if (e.target.classList.contains('arrow-down-btn')) scrollTo(window.innerHeight, 1600);
-  }
-
-  function init() {
-    IS_WIDE_SCREEN = window.innerWidth >= 1024;
-    BORDER = window.innerHeight - (window.innerHeight / 3);
-    if (IS_WIDE_SCREEN) getInitPositions();
-    if (BODY.scrollTop > 0) onScroll();
-  }
-
-  function onResize() {
-    window.clearTimeout(resizeTimeout);
-    resizeTimeout = window.setTimeout(function() {
-      init();
-    }, 300);
-  }
-
-  setTimeout(function() { init() }, 16);
-
-  window.addEventListener('resize', onResize);
-  window.addEventListener('deviceorientation', onDeviceOrientation);
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('scroll', onScroll);
-  document.addEventListener('click', onDocumentClick);
-
-})();
+window.addEventListener('resize', onResize);
+window.addEventListener('deviceorientation', onDeviceOrientation);
+document.addEventListener('mousemove', onMouseMove);
+document.addEventListener('scroll', onScroll);
+document.addEventListener('click', onDocumentClick);
